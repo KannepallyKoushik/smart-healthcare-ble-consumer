b98b6972b763293725e1194b694aea6bf1cfd8f1
Kannepally Koushik <koushik.koushik.kumar@gmail.com>
2021-11-21 17:57:56 +0530

    added little more lines for testing

--- mqtt.py
+++ mqtt.py
@@ -1,20 +1,32 @@
-# mqtt_sub.py - Python MQTT subscribe example 
+# mqtt_sub.py - Python MQTT subscribe example
 #
 import paho.mqtt.client as mqtt
- 
+
+
 def on_connect(client, userdata, flags, rc):
     print("Connected to broker")
- 
+
+
 def on_message(client, userdata, message):
-    data = str(message.payload)
-    print ("Message received: "  + data)
+    data = message.payload
+    topic = message.topic
+    qos = message.qos
+    print("' Received message '" + data.decode('utf-8') + "' on topic '" +
+          topic.decode('utf-8') + "' with QoS " + qos.decode('utf-8'))
+
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print("Unexpected disconnection.")
+
 
 client = mqtt.Client()
 client.username_pw_set("admin1", password='admin1')
-client.connect("localhost", 1883) 
+client.connect("localhost", 1883)
 
-client.on_connect = on_connect       #attach function to callback
-client.on_message = on_message       #attach function to callback
+client.on_connect = on_connect  # attach function to callback
+client.on_message = on_message  # attach function to callback
+client.on_disconnect = on_disconnect
 
-client.subscribe("mq2_mqtt") 
-client.loop_forever()                 #start the loop
+client.subscribe("bp")
+client.loop_forever()  # start the loop
