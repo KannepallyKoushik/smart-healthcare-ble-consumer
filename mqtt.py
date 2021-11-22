e92365589f4c5db7f7afed897961085065b64936
Kannepally Koushik <koushik.koushik.kumar@gmail.com>
2021-11-22 23:11:01 +0530

    Added Handlers for Each Topic and refactored code

--- mqtt.py
+++ mqtt.py
@@ -1,14 +1,38 @@
 import paho.mqtt.client as mqtt
 import pika
 import json
-import time
+
+# Handler Functions for Each topic
+
+
+def handle_bp(data):
+    critical_scores = [3, 2, 1, 0, 1, 2, 3]
+    threshold_ranges = [585, 1170, 1755, 2340, 2925, 3510, 4095]
+    l = len(threshold_ranges)
+
+    for i in range(l):
+        if data <= threshold_ranges[i]:
+            break
+        else:
+            continue
+
+    bp_dict = dict()
+    bp_dict['systolic_blood_pressure'] = critical_scores[i]
+    bp_dict['diastolic_blood_pressure'] = critical_scores[i]
+    bp_json = json.dumps(bp_dict)
+    print("Publishing it to RabbitMQ broker")
+    publish_to_broker("blood_pressure", "bp.*", bp_json)
+
+
+def handle_temp(data):
+    print("Yet to implement")
 
 
 # Functions to Connect to Brokers / Publish / Subscribe (Controllers)
 
 def establish_Channels():
-    topic_name = 'blood_pressure'
-    channel.exchange_declare(exchange=topic_name, exchange_type='topic')
+    topic_name1 = 'blood_pressure'
+    channel.exchange_declare(exchange=topic_name1, exchange_type='topic')
 
 
 def publish_to_broker(topic_name, routing_key, message):
@@ -25,14 +49,15 @@ def on_message(client, userdata, message):
     data = message.payload
     topic = message.topic
     qos = message.qos
-    data = data.decode('utf-8')
-    print("' Received message '", int(data), "' on topic '",
+    data = int(data.decode('utf-8'))
+    topic = topic.decode('utf-8')
+    print("' Received message '", data, "' on topic '",
           str(topic), "' with QoS ", str(qos))
-    bp_dict = dict()
-    bp_dict['systolic_blood_pressure'] = int(data)
-    bp_json = json.dumps(bp_dict)
-    print("Publishing it to RabbitMQ broker")
-    publish_to_broker("blood_pressure", "bp.*", bp_json)
+
+    if topic == 'bp':
+        handle_bp(data)
+    elif topic == 'temp':
+        handle_temp(data)
 
 
 def on_disconnect(client, userdata, rc):
